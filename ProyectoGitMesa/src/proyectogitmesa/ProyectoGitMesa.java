01141fa0ccc045ced8682461c4ead3ff9ec70c1c
guadalupe sirio <guadalupe.sirio@gmail.com>
2021-09-29 21:13:24 -0300

    commit desde rama guada

--- ProyectoGitMesa/src/proyectogitmesa/ProyectoGitMesa.java
+++ ProyectoGitMesa/src/proyectogitmesa/ProyectoGitMesa.java
@@ -6,6 +6,8 @@ public class ProyectoGitMesa {
     public static void main(String[] args) {
         System.out.println("Andrea ");
         System.out.println("Guada ");
+        
+        System.out.println("Combios desde mi rama!");
     }
     
 }
