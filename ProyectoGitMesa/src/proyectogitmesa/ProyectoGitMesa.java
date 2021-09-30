86c764c1ca6cddeee1e6971ff9cca74bcc369c6a
Andrea Reyna <andree.reyna@gmail.com>
2021-09-29 21:10:19 -0300

    Commit main

--- ProyectoGitMesa/src/proyectogitmesa/ProyectoGitMesa.java
+++ ProyectoGitMesa/src/proyectogitmesa/ProyectoGitMesa.java
@@ -4,13 +4,10 @@ package proyectogitmesa;
 public class ProyectoGitMesa {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Modificacion desde nuevaRama ");
-     
-=======
         System.out.println("Andrea ");
         System.out.println("Guada ");
->>>>>>> 35a0407777a90bce431196849ecc4d97bc3992ce
+        System.out.println("Nueva modificacion desde main");
     }
     
 }
